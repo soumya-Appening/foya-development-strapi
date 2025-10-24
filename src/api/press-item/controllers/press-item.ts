e4b15884278e7dce5e60360626883b3460074e1e
soumya@Appening <soumyajeet.c@appening.xyz>
2025-10-24 17:58:14 +0530

    FIX: press item should be sorted by date field

--- src/api/press-item/controllers/press-item.ts
+++ src/api/press-item/controllers/press-item.ts
@@ -2,6 +2,18 @@
  * press-item controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::press-item.press-item');
+export default factories.createCoreController(
+  "api::press-item.press-item",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // Set default sorting by date in descending order (newest first)
+      if (!ctx.query.sort) {
+        ctx.query.sort = "date:desc";
+      }
+
+      return await super.find(ctx);
+    }
+  })
+);
